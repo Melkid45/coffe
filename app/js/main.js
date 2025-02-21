57190ab7349bcd79c21e798e075d6d5925a76ad0
Melkid69 <167970274+Melkid45@users.noreply.github.com>
2025-02-21 11:42:21 +0600

    Main Page

--- app/js/main.js
+++ app/js/main.js
@@ -19,7 +19,44 @@ document.querySelector('.top-btn').addEventListener('click', function(e){
     window.scrollTo(0,0)
 })
 
-document.querySelector('.menu-recipes').addEventListener('click', function(e){
-    document.querySelector('.menu-recipes').classList.toggle('li-active')
-    document.querySelector('.menu__wrapper').classList.toggle('active')
+// document.querySelector('.menu-recipes').addEventListener('click', function(e){
+//     document.querySelectorAll('.menu-recipes').classList.toggle('li-active')
+//     document.querySelectorAll('.menu__wrapper').classList.toggle('active')
+// })
+$('.menu-recipes').on('click', function(e){
+    $(this).toggleClass('li-active')
+    $(this).children('.menu__wrapper').toggleClass('active')
 })
+$('.hamburger').on('click', function(e){
+    if ($(this).children('input').prop('checked')){
+        $('.header__menu').addClass('active')
+        $('.header').addClass('active')
+    }else{
+        $('.header__menu').removeClass('active')
+        $('.header').removeClass('active')
+    }
+})
+
+
+var splide = new Splide( '#program', {
+    perPage: 1,
+    rewind : true,
+    arrows: false
+  } );
+  
+  splide.mount();
+
+  var splide = new Splide( '#popular', {
+    perPage: 1,
+    rewind : true,
+    arrows: false
+  } );
+  
+  splide.mount();
+  var splide = new Splide( '#daily', {
+    perPage: 1,
+    rewind : true,
+    arrows: false
+  } );
+  
+  splide.mount();
